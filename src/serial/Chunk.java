c9ecfd1e700db8fa4b4e8522e3d64bedbb987217
David Oña <dona@stackbuilders.com>
2017-02-05 14:08:36 -0500

    codigo base src de LiteBox

---
+++ src/serial/Chunk.java
@@ -0,0 +1,42 @@
+package serial;
+
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+
+public class Chunk implements Serializable{
+	
+	private byte id;
+	private byte[] name;
+	private byte[] info;
+	private String[] toSyncList;
+		
+	public Chunk() {
+		super();
+		byte[] aux = {0};
+		info = aux;
+	}
+	public int getId() {
+		return (int) id;
+	}
+	public void setId(int id) {
+		this.id = (byte) id;
+	}
+	public String getName() {
+		return new String(name, StandardCharsets.UTF_8);
+	}
+	public void setName(String name) {
+		this.name = name.getBytes(StandardCharsets.UTF_8);
+	}
+	public byte[] getInfo() {
+		return info;
+	}
+	public void setInfo(byte[] info) {
+		this.info = info;
+	}
+	public String[] getToSyncList() {
+		return toSyncList;
+	}
+	public void setToSyncList(String[] toSyncList) {
+		this.toSyncList = toSyncList;
+	}
+}
