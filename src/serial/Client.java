8df194039d76f6ceba21991299ab8fd5ac010a2e
ChrisZoc <lobonop@gmail.com>
2017-02-05 21:42:20 -0500

    added book donwload

--- src/serial/Client.java
+++ src/serial/Client.java
@@ -11,15 +11,24 @@ import javax.swing.border.EmptyBorder;
 import javafx.scene.text.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -30,12 +39,15 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JFileChooser;
+import javax.swing.JSpinner;
 
 public class Client extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
 	private JTextField textField_1;
+	private static Socket soc;
+	private static int port=60020;
+	String host = "localhost";
 
 	/**
 	 * Launch the application.
@@ -58,77 +70,122 @@ public class Client extends JFrame {
 	 */
 	public Client() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 391);
+		setBounds(100, 100, 450, 410);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
+		JButton btnCargar = new JButton("Cargar libro");
+		btnCargar.setEnabled(false);
+		btnCargar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCargar.setBounds(237, 311, 187, 23);
+		contentPane.add(btnCargar);
+
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(105, 280, 97, 20);
+		contentPane.add(spinner);
+
 		JTextArea textArea = new JTextArea();
 		textArea.setEditable(false);
 		textArea.setFont(new java.awt.Font("Monospaced", java.awt.Font.PLAIN, 13));
 		contentPane.add(textArea);
-		
+
 		JScrollPane sp = new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-                JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		textArea.setBounds(58, 11, 327, 179);
 		textArea.setLineWrap(true);
-        textArea.setWrapStyleWord(true);
-        sp.setBounds(20, 30, 404, 174);
+		textArea.setWrapStyleWord(true);
+		sp.setBounds(20, 30, 404, 174);
 		contentPane.add(sp);
-		
-		JButton btnCargarLibros = new JButton("Cargar Libros");
-		btnCargarLibros.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				try {
-					RequestBooks();
-				} catch (ClassNotFoundException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-				private void RequestBooks() throws UnknownHostException, IOException, ClassNotFoundException {
-					ArrayList<String> List =  new ArrayList<String>();
-					Socket soc = new Socket("localhost",60020);	
-					String lista="";
-					Chunk d = null;
-					InputStream o = null;
-					ObjectInput s = null;
-					FileOutputStream fos = null;
-					o = soc.getInputStream();
-					s = new ObjectInputStream(o);
-					d = (Chunk) s.readObject();
-					List = d.getList();
-					int i = 0;
-					String aux="";
-					for(String list:List){
-						aux=list+"                                ";
-						
-						lista+=aux.substring(0, 25)+"código del libro:" + i + "\n";
-						i++;
-					}
-					textArea.setText(lista);
-					
-					
-				}
-			});
-		btnCargarLibros.setBounds(165, 215, 114, 23);
-		contentPane.add(btnCargarLibros);
-		
-		textField = new JTextField();
-		textField.setBounds(116, 309, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
 		JLabel lblCdigoDeLibro = new JLabel("C\u00F3digo de Libro");
-		lblCdigoDeLibro.setBounds(20, 312, 86, 14);
+		lblCdigoDeLibro.setBounds(13, 283, 97, 14);
 		contentPane.add(lblCdigoDeLibro);
-		
+
+		JButton btnDescargar = new JButton(" Descargar libro");
+		btnDescargar.setEnabled(false);
+		btnDescargar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OutputStream os;
+				try {
+					Chunk d = null;
+					soc =new Socket(host, 60021);
+					FileOutputStream fos = null;
+					OutputStream o = soc.getOutputStream();
+					ObjectOutput s = new ObjectOutputStream(o);
+					d= new Chunk();
+					d.setName((String.valueOf(spinner.getValue())));
+					d.setId(0);
+					s.writeObject(d);
+					s.flush();
+					s.close();
+					System.out.println("Book requested code : "+ d.getName());
+					
+					ServerSocket receive=new ServerSocket(60022);
+					
+					InputStream o1 = null;
+					ObjectInput s1 = null;
+					FileOutputStream fos1 = null;
+					soc=receive.accept();
+					o1 = soc.getInputStream();
+					s1 = new ObjectInputStream(o1);
+					d  = (Chunk) s1.readObject();
+				    JFileChooser chooser = new JFileChooser();
+				    chooser.setDialogTitle("Specify a file to save");
+				    chooser.setName(d.getName());
+				    chooser.setSelectedFile( new File(d.getName()));
+				    int retrival = chooser.showSaveDialog(null);
+				    if (retrival == JFileChooser.APPROVE_OPTION) {
+				        try {
+				        	fos1 = new FileOutputStream(chooser.getSelectedFile());
+				            fos1.write(d.getInfo());
+				            fos1.close();
+				        } catch (Exception ex) {
+				            ex.printStackTrace();
+				        }
+				    }
+					
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}finally
+				{
+					//Closing the socket
+					try
+					{
+						soc.close();
+					}
+					catch(Exception e1)
+					{
+						e1.printStackTrace();
+					}
+				}
+
+			}
+		});
+		btnDescargar.setBounds(20, 311, 182, 23);
+		contentPane.add(btnDescargar);
+
 		JButton btnDescargarUnLibro = new JButton("Descargar un libro");
-		btnDescargarUnLibro.setBounds(38, 265, 145, 23);
+		btnDescargarUnLibro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				spinner.setEnabled(true);
+				btnDescargar.setEnabled(true);
+			}
+		});
+		btnDescargarUnLibro.setEnabled(false);
+		btnDescargarUnLibro.setBounds(20, 249, 182, 23);
 		contentPane.add(btnDescargarUnLibro);
-		
-		JButton btnCargarUnLibro = new JButton("Cargar un libro");
+
+		JButton btnCargarUnLibro = new JButton("Escojer un libro para subir");
+		btnCargarUnLibro.setEnabled(false);
 		btnCargarUnLibro.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				String userhome = System.getProperty("user.home");
@@ -139,19 +196,70 @@ public class Client extends JFrame {
 
 					File fichero=fc.getSelectedFile();
 					textField_1.setText(fichero.getName());
+					btnCargar.setEnabled(true);
 				}
 			}
 		});
-		btnCargarUnLibro.setBounds(267, 265, 137, 23);
+		btnCargarUnLibro.setBounds(237, 249, 187, 23);
 		contentPane.add(btnCargarUnLibro);
-		
+
 		textField_1 = new JTextField();
-		textField_1.setBounds(267, 309, 137, 20);
+		textField_1.setEnabled(false);
+		textField_1.setBounds(237, 280, 187, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
-		
-		
-		 
+
+
+		JButton btnCargarLibros = new JButton("Cargar Libros");
+		btnCargarLibros.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					RequestBooks();
+				} catch (ClassNotFoundException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			private void RequestBooks() throws UnknownHostException, IOException, ClassNotFoundException {
+				ArrayList<String> List =  new ArrayList<String>();
+				soc = new Socket(host,port);	
+				String lista="";
+				Chunk d = null;
+				InputStream o = null;
+				ObjectInput s = null;
+				FileOutputStream fos = null;
+				o = soc.getInputStream();
+				s = new ObjectInputStream(o);
+				d = (Chunk) s.readObject();
+				List = d.getList();
+				int i = 0;
+				String aux="";
+				for(String list:List){
+					aux=list+"                                ";
+
+					lista+=aux.substring(0, 25)+"código del libro:" + i + "\n";
+					i++;
+				}
+				textArea.setText(lista);
+				btnCargarLibros.setEnabled(false);
+				btnCargarUnLibro.setEnabled(true);
+				btnDescargarUnLibro.setEnabled(true);
+
+
+			}
+		});
+		btnCargarLibros.setBounds(165, 215, 114, 23);
+		contentPane.add(btnCargarLibros);
+
+
+
+
+
+
+
+
+
+
 	}
-	}
+}
 
--- src/serial/ServerThread.java
+++ src/serial/ServerThread.java
@@ -11,11 +11,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+
 
 public class ServerThread   {
 	private Thread runner;
@@ -33,9 +36,10 @@ public class ServerThread   {
 		System.out.println("Initializing ServerThread...");
 		sendBooksList(soc);
 	}
-	
+
 	private void sendBooksList(Socket skt) {
 		ArrayList<String> my =  new ArrayList<String>();
+
 		sharedFolder = new File(sharedfolder);
 		if (!sharedFolder.exists()) {
 			sharedFolder.mkdirs();
@@ -54,17 +58,125 @@ public class ServerThread   {
 		List.setId(0);
 		List.setName("List");
 		List.setList(my); 
+
 		try {
 			OutputStream o = soc.getOutputStream();
 			ObjectOutput s = new ObjectOutputStream(o);
 			s.writeObject(List);
 			s.flush();
 			s.close();
-			System.out.println("Terminating ClientThread...");
+			System.out.println("Terminating ServerThread...");
+			listenBook(my);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.out.println("Error during serialization");
 			System.exit(1);
 		}
+	}
+
+	private void listenBook(ArrayList<String> my) throws ClassNotFoundException {
+		ServerSocket ser = null;
+		int port=60021;
+		try {
+			ser = new ServerSocket(port);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: " + 60021 + ".");
+			System.exit(1);
+		}
+
+		Socket clientSocket = null;
+		try {
+			while (true) {
+				System.out.println("escuchando");
+				clientSocket = ser.accept();
+				System.out.println("aceptado");
+				validate(clientSocket,my);
+			}
+		} catch (IOException e) {
+			System.err.println("Accept failed.");
+			System.exit(1);
+		}
+		try {
+			ser.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	private void validate(Socket clientSocket, ArrayList<String> my) throws IOException, ClassNotFoundException {
+		System.out.println("validando");
+		Chunk d=null;
+		InputStream o1 = null;
+		ObjectInput s1 = null;
+		FileOutputStream fos = null;
+		
+		o1 = clientSocket.getInputStream();
+		System.out.println("01");
+		s1 = new ObjectInputStream(o1);
+		System.out.println("s1");
+		try {
+			d  = (Chunk) s1.readObject();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if(d.getId()==0){
+			boolean complete = false;
+			Chunk toSend = null;
+			Path path = Paths.get(sharedfolder + "/" + my.get(Integer.parseInt(d.getName())));
+			byte[] data;
+			File archivo = new File(String.valueOf(path));
+			do {
+				System.out.println("File is being copied, waiting...");
+				try {
+					complete = isCompletelyWritten(archivo);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} while (!complete);
+			System.out.println("File ready for transfer, sending...");
+			try {
+				clientSocket=new Socket("localhost",60022);
+				data = Files.readAllBytes(path);
+				toSend = new Chunk();
+				toSend.setInfo(data);
+				toSend.setName(my.get(Integer.parseInt(d.getName())));
+				toSend.setId(0);
+				OutputStream o = clientSocket.getOutputStream();
+				ObjectOutput s = new ObjectOutputStream(o);
+				s.writeObject(toSend);
+				s.flush();
+				s.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(d.getId()==1){
+
+
+		}
+	}
+
+	private boolean isCompletelyWritten(File file) throws InterruptedException  {
+		RandomAccessFile stream = null;
+		try {
+			stream = new RandomAccessFile(file, "rw");
+			return true;
+		} catch (Exception e) {
+			Thread.sleep(1000);
+		} finally {
+			if (stream != null) {
+				try {
+					stream.close();
+				} catch (IOException e) {
+					System.out.println("Exception closing file " + file.getName());
+				}
+			}
+		}
+		return false;
 	}
 }
